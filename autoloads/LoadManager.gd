d662e8a9c7c8795a5785bbfbcb3cb3641b948dba
Grady Wasil <90996993+Arrangedgodly@users.noreply.github.com>
2024-11-05 21:47:33 -0700

    Loading screen mostly works but IDK if it was a good addition lol

--- autoloads/LoadManager.gd
+++ autoloads/LoadManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal progress_changed(progress)
 signal load_done
+signal scene_ready_to_change
 
 var _load_screen_path: String = "res://scenes/loading_screen.tscn"
 var _load_screen = load(_load_screen_path)
@@ -10,18 +11,20 @@ var _scene_path: String
 var _progress: Array = []
 var use_sub_threads: bool = true
 
+func _ready() -> void:
+	set_process(false)
+
 func load_scene(scene_path: String) -> void:
 	_scene_path = scene_path
 	
 	var new_loading_screen = _load_screen.instantiate()
 	get_tree().get_root().add_child(new_loading_screen)
-	
 	self.progress_changed.connect(new_loading_screen._update_progress_bar)
-	self.load_done.connect(new_loading_screen._start_outro_animation)
+	self.load_done.connect(new_loading_screen._start_ready_prompt)
+	self.scene_ready_to_change.connect(new_loading_screen._start_outro_animation)
 	
-	await Signal(new_loading_screen, "loading_screen_has_full_coverage")
-	
-	start_load()
+	new_loading_screen.ls_ready.connect(start_load)
+	new_loading_screen.continue_pressed.connect(_change_scene)
 	
 func start_load() -> void:
 	var state = ResourceLoader.load_threaded_request(_scene_path, "", use_sub_threads)
@@ -40,4 +43,8 @@ func _process(_delta: float) -> void:
 			_loaded_resource = ResourceLoader.load_threaded_get(_scene_path)
 			progress_changed.emit(1.0)
 			load_done.emit()
-			get_tree().change_scene_to_packed(_loaded_resource)
+			set_process(false)
+
+func _change_scene() -> void:
+	scene_ready_to_change.emit()
+	get_tree().change_scene_to_packed(_loaded_resource)
--- scripts/loading_screen.gd
+++ scripts/loading_screen.gd
@@ -1,9 +1,65 @@
 extends CanvasLayer
 
-signal loading_screen_has_full_coverage
+signal ls_ready
+signal continue_pressed
+
+@onready var texture_progress_bar: TextureProgressBar = $ColorRect/TextureProgressBar
+@onready var continue_button: Button = $ColorRect/Button
+@onready var label: Label = $ColorRect/HBoxContainer/Label
+@onready var ellipsis_container: HBoxContainer = $"ColorRect/HBoxContainer/Label/Ellipsis Container"
+@onready var dot1: Label = $"ColorRect/HBoxContainer/Label/Ellipsis Container/Dot1"
+@onready var dot2: Label = $"ColorRect/HBoxContainer/Label/Ellipsis Container/Dot2"
+@onready var dot3: Label = $"ColorRect/HBoxContainer/Label/Ellipsis Container/Dot3"
+
+
+var is_loading: bool = true
+var tween: Tween
+
+func _ready() -> void:
+	continue_button.disabled = true
+	label.text = "Loading"
+	start_dot_animation()
+	await get_tree().create_timer(.1).timeout
+	ls_ready.emit()
 
 func _update_progress_bar(new_value: float) -> void:
-	pass
+	texture_progress_bar.set_value_no_signal(new_value * 100)
 	
+func _start_ready_prompt() -> void:
+	is_loading = false
+	if tween:
+		tween.kill()
+	
+	# Animate the transition to "Ready!"
+	var ready_tween = create_tween()
+	ready_tween.tween_callback(func(): ellipsis_container.hide())
+	ready_tween.tween_property(label, "modulate:a", 0.0, 0.2)
+	ready_tween.tween_callback(func(): label.text = "Ready!")
+	ready_tween.tween_property(label, "modulate:a", 1.0, 0.2)
+	
+	continue_button.disabled = false
+	continue_button.grab_focus()
+	
+func start_dot_animation() -> void:
+	if tween:
+		tween.kill()
+	
+	tween = create_tween().set_loops()
+	
+	# Dot 1 animation
+	tween.parallel().tween_property(dot1, "position:y", -10.0, 0.3).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property(dot1, "position:y", 0.0, 0.3).set_trans(Tween.TRANS_SINE).set_delay(0.3)
+	
+	# Dot 2 animation (delayed start)
+	tween.parallel().tween_property(dot2, "position:y", -10.0, 0.3).set_trans(Tween.TRANS_SINE).set_delay(0.1)
+	tween.parallel().tween_property(dot2, "position:y", 0.0, 0.3).set_trans(Tween.TRANS_SINE).set_delay(0.4)
+	
+	# Dot 3 animation (delayed start)
+	tween.parallel().tween_property(dot3, "position:y", -10.0, 0.3).set_trans(Tween.TRANS_SINE).set_delay(0.2)
+	tween.parallel().tween_property(dot3, "position:y", 0.0, 0.3).set_trans(Tween.TRANS_SINE).set_delay(0.5)
+
 func _start_outro_animation() -> void:
-	pass
+	queue_free()
+
+func _on_button_pressed() -> void:
+	continue_pressed.emit()
